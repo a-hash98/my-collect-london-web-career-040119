9c7cbb35d7d394d93c223990b323357a0c71cbda
Ay Has <ahashim983@yahoo.com>
2019-03-29 01:34:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,1 +1,15 @@
 def my_collect(collecion)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  else
+    return "No block was given!"
+  end
+end
+
+my_collect(["a","b","c"]) do |item|
+  return item.upcase
+end
